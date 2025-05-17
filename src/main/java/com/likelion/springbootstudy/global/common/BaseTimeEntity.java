23d0de5c49cb73a4174d080c4a7d669d6679d169
shinchaerin79 <shinchaerin79@gmail.com>
2025-05-17 11:21:54 +0900

    :sparkles: Feat: JPA 공통 엔티티 BaseTimeEntity 추가

---
+++ src/main/java/com/likelion/springbootstudy/global/common/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.likelion.springbootstudy.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime modifiedAt;
+}
