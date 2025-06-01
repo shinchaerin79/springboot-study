94fbb9e9a63331e80e2f1e382c4e1083383d523f
shinchaerin79 <shinchaerin79@gmail.com>
2025-06-01 15:04:57 +0900

    :bug: Fix: @Transactional 누락으로 인한 리프레시 토큰 저장 문제 해결

--- src/main/java/com/likelion/springbootstudy/domain/auth/service/AuthService.java
+++ src/main/java/com/likelion/springbootstudy/domain/auth/service/AuthService.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -25,6 +26,7 @@ public class AuthService {
   private final UserRepository userRepository;
   private final AuthMapper authMapper;
 
+  @Transactional
   public LoginResponse login(LoginRequest loginRequest) {
     User user = userRepository.findByUsername(loginRequest.getUsername())
         .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
